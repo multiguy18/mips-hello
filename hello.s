37e5aff0ccae57de3f69dbcab96ccadc4c78fbaf
multigy18 <anselm1793@hotmail.com>
2023-03-13 20:51:34 +0100

    Simple user input addition example

--- hello.s
+++ hello.s
@@ -5,67 +5,45 @@
 .global __start
 
 __start:
-    li $v0, 4004
-    li $a0, 1
-    la $a1, testmsg
-    li $a2, 41
-    syscall
-
-    jal testfun
-    nop
-
-    li $v0, 4004
-    li $a0, 1
-    la $a1, newline
-    li $a2, 1
-    syscall
-
-    la $a0, mynumber
-    jal atoi
-    nop
-
-    move $a0, $v0
     
-    la $a1, mybuf
-    jal itoa
-    nop
-
-    li $v0, 4004
-    li $a0, 1
-    la $a1, mybuf
-    li $a2, 11
-    syscall
-
-    li $v0, 4004
-    li $a0, 1
-    la $a1, newline
-    li $a2, 1
-    syscall
-
     li $v0, 4004
     li $a0, 1
     la $a1, prompt
-    li $a2, 24
+    li $a2, 23
     syscall
 
     li $v0, 4003
     li $a0, 0
     la $a1, input
-    li $a2, 30
+    li $a2, 4
     syscall
+
+    la $a0, input
+    jal atoi
+    nop
+
+    add $a0, $v0, 12
+    la $a1, output
+    jal itoa
+    nop
 
     li $v0, 4004
     li $a0, 1
     la $a1, response
-    li $a2, 15
+    li $a2, 26
     syscall
 
     li $v0, 4004
     li $a0, 1
-    la $a1, input
-    li $a2, 80
+    la $a1, output
+    li $a2, 4
     syscall
 
+    li $v0, 4004
+    li $a0, 1
+    la $a1, newline
+    li $a2, 1
+    syscall
 
     li $v0, 4001
     li $a0, 0
@@ -73,11 +51,8 @@ __start:
 
 .section .data
 
-
-testmsg: .asciiz "Testing assembler calls to c and back...\n"
+prompt: .asciiz "Please enter your age: "
+response: .asciiz "\nYour age in 12 years is: "
 newline: .asciiz "\n"
-prompt: .asciiz "Please enter your name: "
-response: .asciiz "\nYour name is: "
-mynumber: .asciiz "933"
-mybuf: .space 12
-input: .space 81
+output: .space 4
+input: .space 4
